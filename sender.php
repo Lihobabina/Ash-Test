6fc289006e5ec754763ccdd528a6412cbfe23a66
Lihobabina <116579066+Lihobabina@users.noreply.github.com>
2023-11-12 10:19:49 +0200

    Update sender.php

--- sender.php
+++ sender.php
@@ -1,28 +1,17 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Получаем данные из формы
-    $userName = $_POST["userName"];
-    $userMail = $_POST["userMail"];
-    $userPhone = $_POST["userPhone"];
+$name = $_POST['userName'];
+$email = $_POST['userMail'];
+$message = $_POST['userPhone'];
 
-    // Адрес, на который отправлять письмо
-    $to = "starshova1309@gmail.com";
+$to = 'starshova1309@gmail.com';
+$date = date ('d.m.y');
+$time = date ('h:i');
+$from = $email;
 
-    // Тема письма
-    $subject = "Новая заявка с формы сайта";
+$msg="
+Имя: $name
+Почта: $email
+Текст: $message";
+mail($to, $msg, "From: $from")
 
-    // Текст письма
-    $message = "Имя: $userName\n";
-    $message .= "Email: $userMail\n";
-    $message .= "Номер телефона: $userPhone\n";
-
-    // Отправляем письмо
-    mail($to, $subject, $message);
-
-    // Возвращаем успешный ответ на клиентскую сторону (можно заменить на другую обработку)
-    echo "Success";
-} else {
-    // Возвращаем ошибку на клиентскую сторону (можно заменить на другую обработку)
-    echo "Error";
-}
 ?>
