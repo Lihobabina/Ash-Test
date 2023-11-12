f76d5551321ddfbff84070113c2519159ec86826
Lihobabina <116579066+Lihobabina@users.noreply.github.com>
2023-11-12 10:23:33 +0200

    Update sender.php

--- sender.php
+++ sender.php
@@ -4,14 +4,19 @@ $email = $_POST['userMail'];
 $message = $_POST['userPhone'];
 
 $to = 'starshova1309@gmail.com';
-$date = date ('d.m.y');
-$time = date ('h:i');
+$date = date('d.m.y');
+$time = date('h:i');
 $from = $email;
 
-$msg="
-Имя: $name
-Почта: $email
-Текст: $message";
-mail($to, $msg, "From: $from")
+$msg = "
+    Имя: $name
+    Почта: $email
+    Текст: $message
+";
 
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From: $from" . "\r\n";
+
+mail($to, "Заявка с сайта", $msg, $headers);
 ?>
